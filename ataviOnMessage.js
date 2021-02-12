18e345355235d9748279b96169f2cb3cee98e195
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-02-12 10:21:41 +0300

    fix: small fix

--- ataviOnMessage.js
+++ ataviOnMessage.js
@@ -1,20 +1,23 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(!sender.tab) {
-            handleMessage(request)
-        }
-    });
+  function (request, sender) {
+    if (!sender.tab) {
+      handleMessage(request)
+    }
+  });
 
-    function handleMessage(message){
-    const $addBtn = document.querySelector('.js-bookmark-add');
-    $addBtn.click();
-    const $inputUrl = document.querySelector('#Bookmark_url');
-    const $inputNameSite = document.querySelector('#Bookmark_caption');
-    const $addSiteBtn = document.querySelector('input.tp-button.fr');
+function handleMessage(message) {
+  const addBtn = document.querySelector('.js-bookmark-add');
+  if (!addBtn) {
+    return;
+  }
+  addBtn.click();
+  const inputUrl = document.querySelector('#Bookmark_url');
+  const inputNameSite = document.querySelector('#Bookmark_caption');
+  const addSiteBtn = document.querySelector('input.tp-button.fr');
 
-    $inputUrl.value = message.url;
-    $inputNameSite.value = message.title;
+  inputUrl.value = message.url;
+  inputNameSite.value = message.title;
 
-    $addSiteBtn.click();
-    setTimeout ( window.close , 1000 );
+  addSiteBtn.click();
+  setTimeout(window.close, 1000);
 }
